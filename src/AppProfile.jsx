46102432c6c9035f6dea8c80433b3bb60160f766
jeonghs <hsjung201558@gmail.com>
2022-10-19 15:55:33 +0900

    profile  생성 및 css 수정

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
 
 function AppProfile() {
-  const name = 'jeong';
-  const list = ['우유', '딸기', '바나나'];
   return (
     <>
-      <h1 className='orange'>Hello! {name}</h1>
-      <h2>Hello!</h2>
-      <p>{name}</p>
-      <ul>
-        {list.map((item)=>(
-          <li>{item}</li>
-        ))}
-      </ul>
-      <img style={{ width:'200px', height:'200px' }} 
-      src='https://avatars.githubusercontent.com/u/97008461?v=4' 
-      alt='nature'></img>
+      <Profile></Profile>
     </>
   );
 }
