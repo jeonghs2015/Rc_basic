d690bafa4cd901b91cabf4320af548813e9073e8
jeonghs <hsjung201558@gmail.com>
2022-10-20 18:20:38 +0900

    total count 기능 추가

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import Counter from './components/Counter'
 
 export default function AppCounter() {
+    const [tots, setTots] = useState(0);
+    const handelClick = () => setTots(prev => prev + 1);
   return (
-    <div>
-        <Counter />
+    <div className='container'>
+        <div className='banner'>
+            Total Count: {tots} {tots > 10 ? 'burning' : 'spiderweb'}
+        </div>
+        <div className='counters'>
+            <Counter total={tots} onClick={handelClick} />
+            <Counter total={tots} onClick={handelClick} />
+        </div>
     </div>
   )
 }
