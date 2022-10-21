dce5b8e6a08881637283ce2978f3a2423865a5a7
jeonghs <hsjung201558@gmail.com>
2022-10-21 11:52:01 +0900

    json 파일 데이터 가져오기

---
+++ src/components/Products.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+export default function Products() {
+    const [count, setCount] = useState(0);
+    const [products, setShowProducts] = useState([]);
+    fetch('data/products.json')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('데이터를 네트워크에서 받아옴');
+            setShowProducts(data);
+        });
+  return (
+    <>
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>
+                    <article>
+                        <h3>{product.name}</h3>
+                        <p>{product.price}</p>
+                    </article>
+                </li>
+            ))}
+        </ul>
+        <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+    </>
+  )
+}
+ 
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import AppJSX from './AppJSX';
 import reportWebVitals from './reportWebVitals';
 import AppProfile from './AppProfile';
 import AppCounter from './AppCounter';
+import AppProducts from './AppProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppCounter />
+    <AppProducts />
   </React.StrictMode>
 );
 
