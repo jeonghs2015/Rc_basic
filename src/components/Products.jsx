61f59d8ac5d028634421b5606feec73a72907559
jeonghs <hsjung201558@gmail.com>
2022-10-21 14:24:51 +0900

    useEffect을 사용하여 세일 상품 보기 기능

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,27 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function Products() {
     const [count, setCount] = useState(0);
-    const [products, setShowProducts] = useState([]);
-    fetch('data/products.json')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log('데이터를 네트워크에서 받아옴');
-            setShowProducts(data);
+    const [products, setProducts] = useState([]);
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => setChecked((prev) => !prev );
+
+    useEffect(() => {
+        fetch(`data/${checked ? 'sale_' : ''}products.json`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('데이터를 네트워크에서 받아옴');
+                setProducts(data);
         });
+        return () => {
+            console.log('청소해주는 일들을 합니다');
+        }
+    }, [checked]);
+   
   return (
     <>
+        <input id='checkbox' type='checkbox' value={checked} onChange={handleChange}></input>
+        <label htmlFor='checkbox'>Show me the money</label>
         <ul>
             {products.map((product) => (
                 <li key={product.id}>
