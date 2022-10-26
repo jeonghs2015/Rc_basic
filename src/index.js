1c648a7da6e099bcb8ada61e19b76bc6aeeaa38b
jeonghs <hsjung201558@gmail.com>
2022-10-26 14:57:31 +0900

    appform 생성

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppMentorsImmer from './AppMentorsImmer';
+import AppForm from './AppForm';
+import AppWrap from './AppWrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsImmer />
+    <AppWrap />
   </React.StrictMode>
 );
 
